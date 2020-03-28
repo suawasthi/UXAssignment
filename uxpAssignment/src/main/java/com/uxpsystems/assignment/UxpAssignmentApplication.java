07fc27a1cf48a774d26ba46110fc1270c21861c9
suawasthi <suawasthi@gmail.com>
2020-03-28 10:07:35 +0530

    commit

--- uxpAssignment/src/main/java/com/uxpsystems/assignment/UxpAssignmentApplication.java
+++ uxpAssignment/src/main/java/com/uxpsystems/assignment/UxpAssignmentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UxpAssignmentApplication {
 
 	public static void main(String[] args) {
+		System.out.println("starting the application");
 		SpringApplication.run(UxpAssignmentApplication.class, args);
 	}
 
