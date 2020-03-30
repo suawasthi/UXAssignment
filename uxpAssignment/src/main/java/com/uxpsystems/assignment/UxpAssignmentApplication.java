fa48c6615987fef8a777edfd428ebc0ba8fbe7c2
suawasthi <suawasthi@gmail.com>
2020-03-30 11:09:46 +0530

    Issue #9

--- uxpAssignment/src/main/java/com/uxpsystems/assignment/UxpAssignmentApplication.java
+++ uxpAssignment/src/main/java/com/uxpsystems/assignment/UxpAssignmentApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UxpAssignmentApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(UxpAssignmentApplication.class, args);
 	}
 
